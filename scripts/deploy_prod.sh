9aade28904311e23c599bdb69aa11754fde54415
Ethan Lane <ethan@vylpes.com>
2023-09-18 11:13:54 +0100

    Add deployment scripts

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -3,20 +3,18 @@
 export PATH="$HOME/.yarn/bin:$PATH"
 export PATH="$HOME/.nodeuse/bin:$PATH"
 
-export BOT_TOKEN=$(cat $HOME/scripts/card-drop/prod_key.txt)
-
 cd ~/apps/card-drop/card-drop_prod \
 && git checkout main \
 && git fetch \
 && git pull \
-&& docker compose --file docker-compose.prod.yml down \
+&& docker compose down \
 && (pm2 stop card-drop_prod || true) \
 && (pm2 delete card-drop_prod || true) \
 && cp .prod.env .env \
 && yarn clean \
 && yarn install --frozen-lockfile \
 && yarn build \
-&& docker compose --file docker-compose.prod.yml up -d \
+&& docker compose up -d \
 && echo "Sleeping for 10 seconds to let database load..." \
 && sleep 10 \
 && yarn run db:up \
