59f48875e4d78195abdcda0fd62758235543271c
Ethan Lane <ethan@vylpes.com>
2023-10-27 22:12:06 +0100

    Add dropnumber test command (#35)

--- src/registry.ts
+++ src/registry.ts
@@ -1,15 +1,24 @@
 import { CoreClient } from "./client/client";
 
+// Global Command Imports
 import About from "./commands/about";
 import Drop from "./commands/drop";
 
+// Test Command Imports
+import Dropnumber from "./commands/442730357897429002/dropnumber";
+
+// Button Event Imports
 import Claim from "./buttonEvents/Claim";
 import Reroll from "./buttonEvents/Reroll";
 
 export default class Registry {
     public static RegisterCommands() {
+        // Global Commands
         CoreClient.RegisterCommand('about', new About());
         CoreClient.RegisterCommand('drop', new Drop());
+
+        // Test Commands
+        CoreClient.RegisterCommand('dropnumber', new Dropnumber(), "442730357897429002");
     }
 
     public static RegisterEvents() {
