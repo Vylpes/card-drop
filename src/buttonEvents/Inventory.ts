ab2b5d70af931b3e79a143d05a53fd502f26bed7
Ethan Lane <ethan@vylpes.com>
2024-02-02 17:54:34 +0000

    Allow viewing other users inventories

--- src/buttonEvents/Inventory.ts
+++ src/buttonEvents/Inventory.ts
@@ -4,11 +4,20 @@ import InventoryHelper from "../helpers/InventoryHelper";
 
 export default class Inventory extends ButtonEvent {
     public override async execute(interaction: ButtonInteraction) {
+        if (!interaction.guild) return;
+
         const userid = interaction.customId.split(" ")[1];
         const page = interaction.customId.split(" ")[2];
 
+        const member = interaction.guild.members.cache.find(x => x.id == userid) || interaction.member;
+
+        if (!member) {
+            await interaction.reply("Unable to find user.");
+            return;
+        }
+
         try {
-            const embed = await InventoryHelper.GenerateInventoryPage(interaction.user.username, userid, Number(page));
+            const embed = await InventoryHelper.GenerateInventoryPage(member.user.username, member.user.id, Number(page));
 
             await interaction.update({
                 embeds: [ embed.embed ],
