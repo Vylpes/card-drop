32cc731442a4084fef4e6730a6a693b4168d5387
Ethan Lane <ethan@vylpes.com>
2024-12-01 20:13:26 +0000

    WIP: Start updating effects command tests

--- tests/commands/effects.test.ts
+++ tests/commands/effects.test.ts
@@ -1,10 +1,42 @@
+import Effects from "../../src/commands/effects";
+
 describe("constructor", () => {
-    test.todo("EXPECT CommandBuilder to be defined");
+    let effects: Effects;
+
+    beforeEach(() => {
+        effects = new Effects();
+    });
+
+    test("EXPECT CommandBuilder to be defined", () => {
+        expect(effects.CommandBuilder).toMatchSnapshot();
+    });
 });
 
 describe("execute", () => {
     describe("GIVEN interaction is not a chat input command", () => {
-        test.todo("EXPECT nothing to happen");
+        let interaction: any;
+
+        let listSpy: any;
+
+        beforeEach(async () => {
+            interaction = {
+                isChatInputCommand: jest.fn().mockReturnValue(false),
+            };
+
+            const effects = new Effects();
+
+            listSpy = jest.spyOn(effects as any, "List");
+
+            await effects.execute(interaction);
+        });
+
+        test("EXPECT isChatInputCommand to have been called", () => {
+            expect(interaction.isChatInputCommand).toHaveBeenCalledTimes(1);
+        });
+
+        test("EXPECT nothing to happen", () => {
+            expect(listSpy).not.toHaveBeenCalled();
+        });
     });
 
     describe("GIVEN subcommand is list", () => {
