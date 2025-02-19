416e30c042429f26c6ab75b529ebc878ef23a237
Ethan Lane <ethan@vylpes.com>
2025-02-19 18:17:16 +0000

    Update tests

--- tests/buttonEvents/Effects/Buy.test.ts
+++ tests/buttonEvents/Effects/Buy.test.ts
@@ -1,7 +1,69 @@
+import {ButtonInteraction} from "discord.js";
+import Buy from "../../../src/buttonEvents/Effects/Buy";
+import GenerateButtonInteractionMock from "../../__functions__/discord.js/GenerateButtonInteractionMock";
+import { ButtonInteraction as ButtonInteractionType } from "../../__types__/discord.js";
+import AppLogger from "../../../src/client/appLogger";
+
+jest.mock("../../../src/client/appLogger");
+
+let interaction: ButtonInteractionType;
+
+beforeEach(() => {
+    jest.resetAllMocks();
+
+    interaction = GenerateButtonInteractionMock();
+    interaction.customId = "effects buy";
+
+});
+
 describe("Execute", () => {
-    test.todo("GIVEN subaction is confirm, EXPECT confirm function executed");
+    test("GIVEN subaction is confirm, EXPECT confirm function executed", async () => {
+        // Arrange
+        interaction.customId += " confirm";
 
-    test.todo("GIVEN subaction is cancel, EXPECT cancel function executed");
+        const confirmSpy = jest.spyOn(Buy as any, "Confirm");
 
-    test.todo("GIVEN subaction is invalid, EXPECT error logged");
+        // Act
+        await Buy.Execute(interaction as unknown as ButtonInteraction);
+
+        // Assert
+        expect(confirmSpy).toHaveBeenCalledTimes(1);
+        expect(confirmSpy).toHaveBeenCalledWith(interaction);
+
+        expect(AppLogger.LogError).not.toHaveBeenCalled();
+    });
+
+    test("GIVEN subaction is cancel, EXPECT cancel function executed", async () => {
+        // Arrange
+        interaction.customId += " cancel";
+
+        const cancelSpy = jest.spyOn(Buy as any, "Cancel");
+
+        // Act
+        await Buy.Execute(interaction as unknown as ButtonInteraction);
+
+        // Assert
+        expect(cancelSpy).toHaveBeenCalledTimes(1);
+        expect(cancelSpy).toHaveBeenCalledWith(interaction);
+
+        expect(AppLogger.LogError).not.toHaveBeenCalled();
+    });
+
+    test("GIVEN subaction is invalid, EXPECT error logged", async () => {
+        // Arrange
+        interaction.customId += " invalid";
+
+        const confirmSpy = jest.spyOn(Buy as any, "Confirm");
+        const cancelSpy = jest.spyOn(Buy as any, "Cancel");
+
+        // Act
+        await Buy.Execute(interaction as unknown as ButtonInteraction);
+
+        // Assert
+        expect(AppLogger.LogError).toHaveBeenCalledTimes(1);
+        expect(AppLogger.LogError).toHaveBeenCalledWith("Buy", "Unknown subaction, effects invalid");
+
+        expect(confirmSpy).not.toHaveBeenCalled();
+        expect(cancelSpy).not.toHaveBeenCalled();
+    });
 });
