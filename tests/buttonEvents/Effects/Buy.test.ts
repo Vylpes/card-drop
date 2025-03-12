89b0b02ef4be316e9fabfba72ec54a1852b75d9c
Ethan Lane <ethan@vylpes.com>
2025-03-12 17:55:51 +0000

    Add tests for effects buy confirm button event

--- tests/buttonEvents/Effects/Buy.test.ts
+++ tests/buttonEvents/Effects/Buy.test.ts
@@ -5,9 +5,11 @@ import { ButtonInteraction as ButtonInteractionType } from "../../__types__/disc
 import AppLogger from "../../../src/client/appLogger";
 import EffectHelper from "../../../src/helpers/EffectHelper";
 import EmbedColours from "../../../src/constants/EmbedColours";
+import User from "../../../src/database/entities/app/User";
 
 jest.mock("../../../src/client/appLogger");
 jest.mock("../../../src/helpers/EffectHelper");
+jest.mock("../../../src/database/entities/app/User");
 
 let interaction: ButtonInteractionType;
 
@@ -81,13 +83,23 @@ describe("Execute", () => {
 });
 
 describe("Confirm", () => {
+    let user: User;
+
     beforeEach(() => {
         interaction.customId += " confirm";
+
+        user = {
+            Currency: 1000,
+            Save: jest.fn(),
+            RemoveCurrency: jest.fn(),
+        } as unknown as User;
+
+        (User.FetchOneById as jest.Mock).mockResolvedValue(user);
     });
 
     test("EXPECT success embed generated", async () => {
         // Assert
-        interaction.customId += " id 1";
+        interaction.customId += " unclaimed 1";
 
         const embed = {
             id: "embed",
@@ -114,7 +126,7 @@ describe("Confirm", () => {
         });
 
         expect(EffectHelper.GenerateEffectBuyEmbed).toHaveBeenCalledTimes(1);
-        expect(EffectHelper.GenerateEffectBuyEmbed).toHaveBeenCalledWith("userId", "id", 1, true);
+        expect(EffectHelper.GenerateEffectBuyEmbed).toHaveBeenCalledWith("userId", "unclaimed", 1, true);
 
         expect(embed.setColor).toHaveBeenCalledTimes(1);
         expect(embed.setColor).toHaveBeenCalledWith(EmbedColours.Success);
@@ -124,6 +136,18 @@ describe("Confirm", () => {
 
         expect(interaction.reply).not.toHaveBeenCalled();
         expect(AppLogger.LogError).not.toHaveBeenCalled();
+
+        expect(User.FetchOneById).toHaveBeenCalledTimes(1);
+        expect(User.FetchOneById).toHaveBeenCalledWith(User, "userId");
+
+        expect(user.RemoveCurrency).toHaveBeenCalledTimes(1);
+        expect(user.RemoveCurrency).toHaveBeenCalledWith(100);
+
+        expect(user.Save).toHaveBeenCalledTimes(1);
+        expect(user.Save).toHaveBeenCalledWith(User, user);
+
+        expect(EffectHelper.AddEffectToUserInventory).toHaveBeenCalledTimes(1);
+        expect(EffectHelper.AddEffectToUserInventory).toHaveBeenCalledWith("userId", "unclaimed", 1);
     });
 
     test("GIVEN id is not supplied, EXPECT error", async () => {
@@ -156,7 +180,7 @@ describe("Confirm", () => {
 
     test("GIVEN quantity is not supplied, EXPECT error", async () => {
         // Assert
-        interaction.customId += " id";
+        interaction.customId += " unclaimed";
 
         const embed = {
             id: "embed",
@@ -186,7 +210,7 @@ describe("Confirm", () => {
 
     test("GIVEN quantity is not a number, EXPECT error", async () => {
         // Assert
-        interaction.customId += " id invalid";
+        interaction.customId += " unclaimed invalid";
 
         const embed = {
             id: "embed",
@@ -216,7 +240,7 @@ describe("Confirm", () => {
 
     test("GIVEN quantity is 0, EXPECT error", async () => {
         // Assert
-        interaction.customId += " id 0";
+        interaction.customId += " unclaimed 0";
 
         const embed = {
             id: "embed",
@@ -244,13 +268,48 @@ describe("Confirm", () => {
         expect(interaction.update).not.toHaveBeenCalled();
     });
 
-    test.todo("GIVEN user is not found, EXPECT error");
+    test("GIVEN user is not found, EXPECT error", async () => {
+        // Assert
+        interaction.customId += " unclaimed 1";
 
-    test.todo("GIVEN user does not have enough currency, EXPECT error");
+        (User.FetchOneById as jest.Mock).mockResolvedValue(undefined);
+
+        // Act
+        await Buy.Execute(interaction as unknown as ButtonInteraction);
+
+        // Assert
+        expect(AppLogger.LogError).toHaveBeenCalledTimes(1);
+        expect(AppLogger.LogError).toHaveBeenCalledWith("Buy Confirm", "Unable to find user");
+
+        expect(EffectHelper.AddEffectToUserInventory).not.toHaveBeenCalled();
+
+        expect(interaction.update).not.toHaveBeenCalled();
+        expect(interaction.reply).not.toHaveBeenCalled();
+    });
+
+    test("GIVEN user does not have enough currency, EXPECT error", async () => {
+        // Assert
+        interaction.customId += " unclaimed 1";
+
+        user.Currency = 0;
+
+        // Act
+        await Buy.Execute(interaction as unknown as ButtonInteraction);
+
+        // Assert
+        expect(interaction.reply).toHaveBeenCalledTimes(1);
+        expect(interaction.reply).toHaveBeenCalledWith("You don't have enough currency to buy this! You have `0 Currency` and need `100 Currency`!");
+        
+        expect(EffectHelper.AddEffectToUserInventory).not.toHaveBeenCalled();
+
+        expect(interaction.update).not.toHaveBeenCalled();
+
+        expect(AppLogger.LogError).not.toHaveBeenCalled();
+    });
 
     test("GIVEN GenerateEffectBuyEmbed returns with a string, EXPECT error replied", async () => {
         // Assert
-        interaction.customId += " id 1";
+        interaction.customId += " unclaimed 1";
         
         (EffectHelper.GenerateEffectBuyEmbed as jest.Mock).mockResolvedValue("Test error");
 
