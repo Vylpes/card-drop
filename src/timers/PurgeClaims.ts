5f64f15ffa9a0c9c2dc4cafd6d9e1fb9777ee4ab
Ethan Lane <ethan@vylpes.com>
2025-04-04 19:03:21 +0100

    Update PurgeClaims timer to purge after 2 minutes

--- src/timers/PurgeClaims.ts
+++ src/timers/PurgeClaims.ts
@@ -4,7 +4,7 @@ import Claim from "../database/entities/app/Claim";
 export default async function PurgeClaims() {
     const claims = await Claim.FetchAll(Claim);
 
-    const whenLastClaimable = new Date(Date.now() - (1000 * 60 * 5)); // 5 minutes ago
+    const whenLastClaimable = new Date(Date.now() - (1000 * 60 * 2)); // 2 minutes ago
 
     const expiredClaims = claims.filter(x => x.WhenCreated < whenLastClaimable);
 
